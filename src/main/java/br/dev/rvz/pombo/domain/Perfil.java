87acca47aac5f1c20d34c54aa308603416344e35
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-29 13:35:06 -0300

    Adicionar anotações de hibernate para mapeamento do banco de dados

--- src/main/java/br/dev/rvz/pombo/domain/Perfil.java
+++ src/main/java/br/dev/rvz/pombo/domain/Perfil.java
@@ -1,17 +1,55 @@
 package br.dev.rvz.pombo.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import br.dev.rvz.pombo.enumarations.Recado;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "perfis")
+@NoArgsConstructor
+@Getter @Setter
+@EqualsAndHashCode(of = {"id"})
 public class Perfil {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length = 80, nullable = false)
 	private String nomeCompleto;
+	
+	@Column(length = 25, nullable = false)
 	private String numeroTelefone;
+	
+	@Column(length = 255)
 	private String fotoPerfil;
+	
+	@Enumerated(EnumType.STRING)
 	private Recado recado;
 	private Boolean ativo = true;
+	
+	public Perfil(Long id, String nomeCompleto, String numeroTelefone, String fotoPerfil, Recado recado,
+			Boolean ativo) {
+		super();
+		this.id = id;
+		this.nomeCompleto = nomeCompleto;
+		this.numeroTelefone = numeroTelefone;
+		this.fotoPerfil = fotoPerfil;
+		this.recado = recado;
+		this.ativo = ativo;
+	}
+	
+	
 	
 }
--- src/main/java/br/dev/rvz/pombo/enumarations/Recado.java
+++ src/main/java/br/dev/rvz/pombo/enumarations/Recado.java
@@ -1,16 +1,31 @@
 package br.dev.rvz.pombo.enumarations;
 
 public enum Recado {
-	DISPONIVEL,
-	OCUPADO,
-	NA_ESCOLA,
-	NO_CINEMA,
-	NO_TRABALHO,
-	BATERIA_PRESTES_A_ACABAR,
-	NAO_POSSO_FALAR_SOMENTE_MENSAGEM,
-	EM_REUNIAO,
-	NA_ACADEMIA,
-	DORMINDO,
-	SO_CHAMADA_URGENTE;
+	DISPONIVEL("Disponível"),
+	OCUPADO("Ocupado"),
+	NA_ESCOLA("Na escola"),
+	NO_CINEMA("No ciname"),
+	NO_TRABALHO("No trabalho"),
+	BATERIA_PRESTES_A_ACABAR("Bateria prestes a acabar!"),
+	NAO_POSSO_FALAR_SOMENTE_MENSAGEM("Não posso falar, somente mensagens!"),
+	EM_REUNIAO("Em reunião"),
+	NA_ACADEMIA("Na academia"),
+	DORMINDO("Dormindo"),
+	SO_CHAMADA_URGENTE("Só chamada urgente!");
+	
+	private String descricao;
+	
+	Recado(String descricao) {
+		this.descricao = descricao;
+	}
 
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
 }
