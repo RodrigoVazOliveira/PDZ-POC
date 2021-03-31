baef9531162bd467c7d9f1b6d52490119d0e96a6
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-31 20:13:25 -0300

    Criar método para buscar perfil pelo número de telefone

--- src/main/java/br/dev/rvz/pombo/services/PerfilService.java
+++ src/main/java/br/dev/rvz/pombo/services/PerfilService.java
@@ -1,8 +1,11 @@
 package br.dev.rvz.pombo.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.dev.rvz.pombo.domain.Perfil;
 import br.dev.rvz.pombo.repositories.PerfilRepository;
 
 @Service
@@ -10,5 +13,15 @@ public class PerfilService {
 
 	@Autowired
 	private PerfilRepository perfilRepository;
+	
+	public Perfil procurarPefilPorNumeroDeTelefone(String numerotelefone) {
+		Optional<Perfil> perfil = perfilRepository.findByNumeroTelefone(numerotelefone);
+		
+		if (!perfil.isPresent()) {
+			throw new RuntimeException("Perfil não localizado com o número " + numerotelefone);
+		}
+		
+		return perfil.get();
+	}
 	
 }
