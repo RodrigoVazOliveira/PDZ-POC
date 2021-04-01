80828d577c3c61b0975efd9a77385a027b0e98f6
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-01 17:26:37 -0300

    Corrigir endpoint busca para buscar contas por telefone

--- src/main/java/br/dev/rvz/pombo/controllers/ContaController.java
+++ src/main/java/br/dev/rvz/pombo/controllers/ContaController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,9 +40,9 @@ public class ContaController {
 		return contaService.obterTodasAsContas();
 	}
 	
-	@GetMapping
+	@GetMapping("busca")
 	@ResponseStatus(HttpStatus.OK)
-	public @ResponseBody Conta procurarContaPorNumeroDeTelefone(@PathParam(value = "numeroTelefone") String numeroTelefone) {
+	public @ResponseBody Conta procurarContaPorNumeroDeTelefone(@RequestParam String numeroTelefone) {
 		try {
 			return contaService.procurarContaPorNumeroDeTelefone(numeroTelefone);
 		} catch (RuntimeException e) {
