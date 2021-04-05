31a4a4296b80983731a7f428c2fbc382aebeb6d2
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-05 14:24:31 -0300

    Criar endpoint para gravar novo contato

--- src/main/java/br/dev/rvz/pombo/controllers/ContatoController.java
+++ src/main/java/br/dev/rvz/pombo/controllers/ContatoController.java
@@ -1,9 +1,10 @@
 package br.dev.rvz.pombo.controllers;
 
+import br.dev.rvz.pombo.domain.Contato;
 import br.dev.rvz.pombo.services.ContatoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("contatos/")
@@ -11,4 +12,10 @@ public class ContatoController {
 
     @Autowired
     private ContatoService contatoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Contato gravarNovoContato(@RequestBody Contato contato) {
+        return contatoService.gravarNovoContato(contato);
+    }
 }
