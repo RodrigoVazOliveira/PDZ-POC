64141810a68aa406d20afcb2a49dbecfc3a8a6b7
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-05 14:20:56 -0300

    Criar controller para mapear funcionalidade do contato

--- src/main/java/br/dev/rvz/pombo/controllers/ContatoController.java
+++ src/main/java/br/dev/rvz/pombo/controllers/ContatoController.java
@@ -1,2 +1,14 @@
-package br.dev.rvz.pombo.controllers;public class ContatoController {
+package br.dev.rvz.pombo.controllers;
+
+import br.dev.rvz.pombo.services.ContatoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("contatos/")
+public class ContatoController {
+
+    @Autowired
+    private ContatoService contatoService;
 }
