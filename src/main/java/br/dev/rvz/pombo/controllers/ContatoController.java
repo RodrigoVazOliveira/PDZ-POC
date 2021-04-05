abeb8c7e59deed69010ac6aa05584d6cd2c93ebc
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-05 14:20:38 -0300

    Criar método para salvar contatos

---
+++ src/main/java/br/dev/rvz/pombo/controllers/ContatoController.java
@@ -0,0 +1,2 @@
+package br.dev.rvz.pombo.controllers;public class ContatoController {
+}
--- src/main/java/br/dev/rvz/pombo/services/ContatoService.java
+++ src/main/java/br/dev/rvz/pombo/services/ContatoService.java
@@ -1,5 +1,6 @@
 package br.dev.rvz.pombo.services;
 
+import br.dev.rvz.pombo.domain.Contato;
 import br.dev.rvz.pombo.repositories.ContatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class ContatoService {
 
     @Autowired
     private ContatoRepository contatoRepository;
+
+    public Contato gravarNovoContato(Contato contato) throws RuntimeException {
+        return contatoRepository.save(contato);
+    }
 }
