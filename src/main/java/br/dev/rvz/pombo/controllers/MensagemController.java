4d333f1c687820c22c93d90262be9b3186da7ad1
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-06 11:07:31 -0300

    Criar método para enviar mensagem

---
+++ src/main/java/br/dev/rvz/pombo/controllers/MensagemController.java
@@ -0,0 +1,21 @@
+package br.dev.rvz.pombo.controllers;
+
+import br.dev.rvz.pombo.domain.Mensagem;
+import br.dev.rvz.pombo.services.MensagemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("mensagens/")
+public class MensagemController {
+
+    @Autowired
+    private MensagemService mensagemService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mensagem enviarNovaMensagem(@RequestBody Mensagem mensagem) {
+        mensagemService.enviarNovaMensagem(mensagem);
+    }
+}
--- src/main/java/br/dev/rvz/pombo/services/MensagemService.java
+++ src/main/java/br/dev/rvz/pombo/services/MensagemService.java
@@ -1,12 +1,21 @@
 package br.dev.rvz.pombo.services;
 
+import br.dev.rvz.pombo.domain.Mensagem;
 import br.dev.rvz.pombo.repositories.MensagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Service
 public class MensagemService {
 
     @Autowired
     private MensagemRepository mensagemRepository;
+
+    public Mensagem enviarNovaMensagem(Mensagem mensagem) {
+        mensagem.setDataHora(LocalDateTime.now());
+        return mensagemRepository.save(mensagem);
+    }
 }
